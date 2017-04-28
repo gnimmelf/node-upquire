4e894f36933d9f980307121d573963b064464b41
Flemming hansen <gnimmelf@gmail.com>
2017-04-28 22:16:10 +0200

    Update index.js
    Added options for `pathOnly` and `dirnameOnly`

--- index.js
+++ index.js
@@ -5,25 +5,31 @@ var callerId = require('caller-id');
 function isModulePath(modulePath) {
   //console.log(modulePath)
   return ( fs.existsSync( modulePath )
-          || fs.existsSync( modulePath+'.js' ) );  
+          || fs.existsSync( modulePath+'.js' ) );
 }
 
-module.exports = function(modulePath, options) {  
-  var basePath = path.dirname( callerId.getData().filePath );  
+module.exports = function(modulePath, options) {
+  var basePath = path.dirname( callerId.getData().filePath );
   if (basePath == '.') {
     // We're in the repl(?)
     var basePath = process.env.PWD;
-  } 
-  var baseParts = basePath.split(path.sep);  
+  }
+  var baseParts = basePath.split(path.sep);
   while (baseParts.length && !isModulePath( path.join( basePath, modulePath ) ) ) {
     baseParts.pop();
     basePath = baseParts.join(path.sep);
   };
 
+  var requirePath = path.join( basePath, modulePath );
   if (options && options.pathOnly) {
-    return path.join( basePath, modulePath);
-  } 
+
+    if (options.dirnameOnly && fs.lstatSync(requirePath).isFile()) {
+      requirePath = path.dirname(requirePath)
+    }
+
+    return requirePath;
+  }
   else {
-    return require( path.join( basePath, modulePath ) );    
-  } 
+    return require( requirePath );
+  }
 };
